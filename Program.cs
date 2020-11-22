370eb876ba4494a5f7ee961cb26740b655aecf6f
Chris <kizaing@gmail.com>
2020-11-22 01:33:17 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot;
+using Impostor.Api;
+
+namespace ImpostorBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var botClient = new TelegramBotClient("1476247742:AAGGDzhiqt7Wf3m-uz6IhM1QzVJGllFkVBE");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
