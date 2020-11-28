9ba18decb7f99ca4c72a9444f392ea33e8a8b864
Chris <kizaing@gmail.com>
2020-11-28 00:35:35 +0000

    Started telegram bot text reading

--- Telegram/telegram.cs
+++ Telegram/telegram.cs
@@ -1,17 +1,31 @@
 using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace Impostor.Plugins.ImpostorTeleBot.Telegram
 {
-    
-
     public class Bot
     {
+        static ITelegramBotClient botClient;
+
         public static Config config;
 
         public Bot(Config _config) {
 
             config = _config;
-            TelegramBotClient botClient = new TelegramBotClient(config.Token);
+            botClient = new TelegramBotClient(config.Token);
+
+            var me = botClient.GetMeAsync().Result;
+
+            botClient.OnMessage += Bot_OnMessage;
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e) {
+            if(e.Message.Text != null) {
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text:   "You said:\n" + e.Message.Text
+                );
+            }
         }
     }
 }
